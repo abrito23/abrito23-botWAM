660c3377f437a9e91a9a646aab19fb5c5703a5fd
abrito23 <154318747+abrito23@users.noreply.github.com>
2023-12-19 16:09:08 -0700

    Update app.js
    agregar puertos 300X

--- app.js
+++ app.js
@@ -76,7 +76,7 @@ const mainBot1 = async () => {
         database: adapterDB,
     })
 
-    QRPortalWeb({name:BOTNAME})
+    QRPortalWeb({name:BOTNAME,port:3001})
 }
 
 
@@ -94,7 +94,7 @@ const mainBot2 = async () => {
         database: adapterDB,
     })
 
-    QRPortalWeb({name:BOTNAME})
+    QRPortalWeb({name:BOTNAME,port:3002})
 }
 
 mainBot1()
