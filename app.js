b3dfbdda13ff43f733211a1a9a1bc896030ea6ff
abrito23 <154318747+abrito23@users.noreply.github.com>
2023-12-29 00:49:38 -0700

    Update app.js

--- app.js
+++ app.js
@@ -142,5 +142,20 @@ const flowPrincipal = addKeyword(['hola', 'ole', 'alo'])
     
         QRPortalWeb({name:BOTNAME})
     }
+    const main2 = async () => {
+        const BOTNAME = 'bot-2'
+        const adapterDB = new MockAdapter()
+        const adapterFlow = createFlow([flowPrincipal])
+        const adapterProvider = createProvider(BaileysProvider,{name:BOTNAME})
+    
+        createBot({
+            flow: adapterFlow,
+            provider: adapterProvider,
+            database: adapterDB,
+        })
+    
+        QRPortalWeb({name:BOTNAME})
+    }
 
     main()
+    main2()
